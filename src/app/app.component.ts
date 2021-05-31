8d32946740f4ae9fac9a917bc5516ab2121b355c
Aguusquiroga <quiroga.aguus1@gmail.com>
2021-05-31 15:04:24 -0300

    subiendo archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { EditorChangeContent, EditorChangeSelection } from 'ngx-quill';
+import {FormsModule} from '@angular/forms';
+import { OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControlName } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
   title = 'editorCk';
-}
+  editorText = "";
+
+  ngOnInit(){
+
+
+  }
+  changedEditor(event: EditorChangeContent | EditorChangeSelection){
+    console.log('estas escribiendo'+event);
+    this.editorText=event['editor']['root']['innerHTML'];
+  }
+
+
+  }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {QuillModule} from 'ngx-quill';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, QuillModule.forRoot(), FormsModule, ReactiveFormsModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
